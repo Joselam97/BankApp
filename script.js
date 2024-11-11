eae2d7f3357eac273ded37a754e2b4310d4d86d2
Jose Alvarez <jlam97@hotmail.es>
2024-11-11 16:52:29 -0600

    Creacion de funcion para crear usernames para cada cuenta

--- script.js
+++ script.js
@@ -6,75 +6,118 @@
 
 // Data
 const account1 = {
-    owner: 'Jose Alvarez',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, // %
-    pin: 1111,
-  };
-  
-  const account2 = {
-    owner: 'Yesenia Morales',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
-  };
-  
-  const account3 = {
-    owner: 'Molly Smith',
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
-  };
-  
-  const account4 = {
-    owner: 'Sarah Williams',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
-  };
-  
-  const accounts = [account1, account2, account3, account4];
-  
-  // Elements
-  const labelWelcome = document.querySelector('.welcome');
-  const labelDate = document.querySelector('.date');
-  const labelBalance = document.querySelector('.balance__value');
-  const labelSumIn = document.querySelector('.summary__value--in');
-  const labelSumOut = document.querySelector('.summary__value--out');
-  const labelSumInterest = document.querySelector('.summary__value--interest');
-  const labelTimer = document.querySelector('.timer');
-  
-  const containerApp = document.querySelector('.app');
-  const containerMovements = document.querySelector('.movements');
-  
-  const btnLogin = document.querySelector('.login__btn');
-  const btnTransfer = document.querySelector('.form__btn--transfer');
-  const btnLoan = document.querySelector('.form__btn--loan');
-  const btnClose = document.querySelector('.form__btn--close');
-  const btnSort = document.querySelector('.btn--sort');
-  
-  const inputLoginUsername = document.querySelector('.login__input--user');
-  const inputLoginPin = document.querySelector('.login__input--pin');
-  const inputTransferTo = document.querySelector('.form__input--to');
-  const inputTransferAmount = document.querySelector('.form__input--amount');
-  const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-  const inputCloseUsername = document.querySelector('.form__input--user');
-  const inputClosePin = document.querySelector('.form__input--pin');
-  
-  /////////////////////////////////////////////////
-  /////////////////////////////////////////////////
-  // LECTURES
-  
-  const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-  ]);
-  
-  const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-  
-  /////////////////////////////////////////////////
+  owner: 'Jose Alvarez',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
 
-  let arr = ['a','b','c','d','e'];
+const account2 = {
+  owner: 'Yesenia Morales',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
 
-console.log(arr.slice(2));
+const account3 = {
+  owner: 'Molly Smith',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Williams',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+            <div class="movements__type movements__type--${type}">
+            ${i + 1} ${type}
+            </div>
+            <div class="movements__value">${mov}</div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+
+const createUserNames = function (accs) {
+  accs.forEach(function(acc) {
+    acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('');
+  })
+};
+
+createUserNames(accounts);
+console.log(accounts)
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// Currencies
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+const eurToUsd = 1.1;
+
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `withdrew`} ${Math.abs(
+      mov
+    )}`
+);
+
