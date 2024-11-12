ccb114aeb434461e3ac04aaad1a23bd1af7fdd40
Jose Alvarez <jlam97@hotmail.es>
2024-11-11 20:46:46 -0600

    Implementacion de metodo para mostrar los incomes, outcomes e interests

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const displayMovements = function (movements) {
             <div class="movements__type movements__type--${type}">
             ${i + 1} ${type}
             </div>
-            <div class="movements__value">${mov}</div>
+            <div class="movements__value">${mov}€</div>
         </div>
     `;
 
@@ -84,9 +84,33 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function(movements){
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+
+const calcDisplaySummary = function(movements){
+  const incomes = movements
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements 
+  .filter(mov => mov < 0)
+  .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interests = movements
+  .filter(mov => mov > 0)
+  .map(deposit => (deposit * 1.2)/100)
+  .filter((int, i, arr) => {
+    console.log(arr);
+    return int >= 1;
+  })
+  .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interests}€`
+};
+calcDisplaySummary(account1.movements);
 
 
 const createUserNames = function (accs) {
