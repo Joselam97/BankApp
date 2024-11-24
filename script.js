2ad4ae0572e8f3e4371b3564b81a6537924f925a
Jose Alvarez <jlam97@hotmail.es>
2024-11-23 20:01:33 -0600

    Implementacion de fechas en la app, para current balance, loans y movements summary

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-11-18T21:31:17.178Z',
+    '2024-12-23T07:42:02.383Z',
+    '2024-01-28T09:15:04.904Z',
+    '2024-04-01T10:17:24.185Z',
+    '2024-05-08T14:11:59.604Z',
+    '2023-05-27T17:01:17.194Z',
+    '2022-07-11T23:36:17.929Z',
+    '2022-07-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT',
@@ -32,14 +32,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2023-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2024-02-05T16:33:06.386Z',
+    '2024-04-10T14:43:26.374Z',
+    '2024-06-25T18:49:59.371Z',
+    '2024-07-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -50,6 +50,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2024-09-01T13:15:33.035Z',
+    '2024-05-30T09:48:16.867Z',
+    '2024-04-25T06:04:23.907Z',
+    '2024-10-25T14:18:46.235Z',
+    '2022-12-05T16:33:06.386Z',
+    '2022-10-10T14:43:26.374Z',
+    '2023-09-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -57,6 +70,19 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    '2024-10-18T21:31:17.178Z',
+    '2024-10-23T07:42:02.383Z',
+    '2024-11-28T09:15:04.904Z',
+    '2024-12-01T10:17:24.185Z',
+    '2024-04-08T14:11:59.604Z',
+    '2024-05-27T17:01:17.194Z',
+    '2023-07-11T23:36:17.929Z',
+    '2023-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -87,23 +113,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-
 //Muestra resumen de transacciones
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => 
-  a - b) : movements;
-
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
         <div class="movements__row">
             <div class="movements__type movements__type--${type}">
             ${i + 1} ${type}
             </div>
+            <div class="movements__date">${displayDate}</div>
             <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>
     `;
@@ -112,13 +144,11 @@ const displayMovements = function (movements, sort = false) {
   });
 };
 
-
 //Muestra el balance
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
-
 
 //Muestra el resumen (Income, Outcome, Interest)
 const calcDisplaySummary = function (acc) {
@@ -143,7 +173,6 @@ const calcDisplaySummary = function (acc) {
   labelSumInterest.textContent = `${interests.toFixed(2)}€`;
 };
 
-
 //Crea un username con la inicial de nombre y apellido
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -156,20 +185,24 @@ const createUserNames = function (accs) {
 };
 createUserNames(accounts);
 
-
 //Funcion que muestra informacion de la cuenta
 const updateUI = function (acc) {
   //Muestra transacciones
-  displayMovements(acc.movements);
+  displayMovements(acc);
   //Muestra balance
   calcDisplayBalance(acc);
   //Muestra resumen
   calcDisplaySummary(acc);
 };
 
-
 //Manejo de evento con Login
 let currentAccount;
+
+//Loggin falso siempre
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
 
 //Funcion para Login
 btnLogin.addEventListener('click', function (e) {
@@ -190,6 +223,17 @@ btnLogin.addEventListener('click', function (e) {
     //muestra el estilo css una vez logged
     containerApp.style.opacity = 100;
 
+
+    // Crea la fecha actual
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+
     //Limpia info del user en text inputs
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -197,7 +241,6 @@ btnLogin.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
-
 
 //Implementa Transferencias
 btnTransfer.addEventListener('click', function (e) {
@@ -220,10 +263,14 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    //Agrega fecha de transferencia
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
+    //Actualiza el design
     updateUI(currentAccount);
   }
 });
-
 
 //Solicita Loan
 btnLoan.addEventListener('click', function (e) {
@@ -231,17 +278,18 @@ btnLoan.addEventListener('click', function (e) {
 
   const amount = Math.floor(inputLoanAmount.value);
 
-  if (amount > 0 && currentAccount.movements.some(mov => 
-    mov >= amount * 0.1)) {
-      //Agrega movimiento
-      currentAccount.movements.push(amount);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Agrega movimiento
+    currentAccount.movements.push(amount);
 
-      //Restablece estilo
-      updateUI(currentAccount)
-    }
-    inputLoanAmount.value = '';
+    //Agrega fecha al Loan
+    currentAccount.movementsDates.push(new Date().toISOString());
+
+    //Restablece estilo
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
 });
-
 
 //Borrar cuentas
 btnClose.addEventListener('click', function (e) {
@@ -265,17 +313,14 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
-
 //Funcionalidad para ordenar transacciones
 let sorted = false;
-btnSort.addEventListener('click', function(e){
+btnSort.addEventListener('click', function (e) {
   e.preventDefault();
 
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
-
-
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
