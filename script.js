dfaa63ec96d944c6c869eb2fc777b1907eae4ce0
Jose Alvarez <jlam97@hotmail.es>
2024-11-23 20:59:53 -0600

    condicion para agregar cuantos dias antes se hizo una transaccion

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ const account1 = {
     '2024-12-23T07:42:02.383Z',
     '2024-01-28T09:15:04.904Z',
     '2024-04-01T10:17:24.185Z',
-    '2024-05-08T14:11:59.604Z',
-    '2023-05-27T17:01:17.194Z',
-    '2022-07-11T23:36:17.929Z',
-    '2022-07-12T10:51:36.790Z',
+    '2024-11-22T14:11:59.604Z',
+    '2024-11-20T17:01:17.194Z',
+    '2024-12-05T23:36:17.929Z',
+    '2024-12-02T10:32:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT',
@@ -36,10 +36,10 @@ const account2 = {
     '2023-11-30T09:48:16.867Z',
     '2023-12-25T06:04:23.907Z',
     '2023-01-25T14:18:46.235Z',
-    '2024-02-05T16:33:06.386Z',
-    '2024-04-10T14:43:26.374Z',
-    '2024-06-25T18:49:59.371Z',
-    '2024-07-26T12:01:20.894Z',
+    '2024-11-17T14:11:59.604Z',
+    '2024-11-28T17:01:17.194Z',
+    '2024-12-02T23:36:17.929Z',
+    '2024-12-04T10:32:36.790Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -56,10 +56,10 @@ const account3 = {
     '2024-05-30T09:48:16.867Z',
     '2024-04-25T06:04:23.907Z',
     '2024-10-25T14:18:46.235Z',
-    '2022-12-05T16:33:06.386Z',
-    '2022-10-10T14:43:26.374Z',
-    '2023-09-25T18:49:59.371Z',
-    '2023-07-26T12:01:20.894Z',
+    '2024-11-25T14:11:59.604Z',
+    '2024-11-30T17:01:17.194Z',
+    '2024-12-04T23:36:17.929Z',
+    '2024-12-02T10:32:36.790Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -76,10 +76,10 @@ const account4 = {
     '2024-10-23T07:42:02.383Z',
     '2024-11-28T09:15:04.904Z',
     '2024-12-01T10:17:24.185Z',
-    '2024-04-08T14:11:59.604Z',
-    '2024-05-27T17:01:17.194Z',
-    '2023-07-11T23:36:17.929Z',
-    '2023-07-12T10:51:36.790Z',
+    '2024-12-03T14:11:59.604Z',
+    '2024-11-30T17:01:17.194Z',
+    '2024-12-02T23:36:17.929Z',
+    '2024-12-05T10:32:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT',
@@ -113,6 +113,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+// Funcion para dar formato a la fecha
+const formatMovementDate = function (date) {
+  const calcDisplayPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDisplayPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
+
 //Muestra resumen de transacciones
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -125,10 +144,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
         <div class="movements__row">
@@ -202,6 +218,11 @@ let currentAccount;
 currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
+
+
+//Internationalizing API
+const now = new Date();
+labelDate.textContent = new Intl.DateTimeFormat()
 
 
 //Funcion para Login
