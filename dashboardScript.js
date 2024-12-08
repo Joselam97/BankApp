9a5c946ea15a64ef5e3dcc82549e3186c1fcb6ed
Jose Alvarez <jlam97@hotmail.es>
2024-12-07 20:11:03 -0600

    Desvanecimiento al NavBar y Sticky Navbar al momento de hacer scrolldown

--- dashboardScript.js
+++ dashboardScript.js
@@ -8,8 +8,14 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const nav = document.querySelector('.nav');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsConatiner = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 ///////////////////////////////////////
-// Modal window
+// Modal Window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -55,10 +61,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 ///////////////////////////////////////
 //Tabbed Component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsConatiner = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsConatiner.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -76,4 +78,36 @@ tabsConatiner.addEventListener('click', function (e) {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+//Menu Fade Animation
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//Pasing 'argument' into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+//Sticky Navigation
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY > initialCoords.top) 
+    nav.classList.add('sticky')
+  else
+   nav.classList.remove('sticky');
 });
